cf020167efd1d4edd282cbe109a94c57ffcb0ef7
tom <zaisenghan1361@gmail.com>
2022-11-11 13:03:26 +0630

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,zh-CN', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false}, 'google_translate_element');
+    if(typeof(document.querySelector) == 'function') {
+        document.querySelector('.goog-logo-link').setAttribute('style', 'display: none');
+        document.querySelector('.goog-te-gadget').setAttribute('style', 'font-size: 0');
+    }
+}
