e7c3b2a25183d988144d2637f1f131c8f9c286d6
tom <zaisenghan1361@gmail.com>
2022-11-12 11:21:35 +0630

    third commit

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ const data2 = {
        data: [88, 12],
        backgroundColor: [
        '#fff',
-       '#FFA500'
+       '#333'
        ]
    }]
    };          
@@ -87,4 +87,95 @@ data: data3,
 options: {
     responsive: true
 }
-});    
+});
+
+
+const labels =['jan','feb','march','apr','may','jun','july']
+const profile_data = {
+  labels: labels,
+  datasets: [{
+    label: 'Post',
+    data: [15, 59, 80, 81, 56, 55, 60],
+    fill: false,    
+    borderColor: '#50C878',
+    tension: 0.1
+  },
+  {
+    label: 'Transaction',
+    data: [10, 20, 25, 20, 15, 30, 40],
+    fill: false,    
+    borderColor: '#6870fa',
+    tension: 0.1
+  }
+    ]}
+
+var profile_line_chart = new Chart(document.getElementById("profile-line").getContext("2d"),
+{
+type: 'line',
+data: profile_data,
+options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    interaction: {
+        mode: 'index',
+        intersect: false,
+      },
+      stacked: false,
+      plugins: {
+        title: {
+          display: true,
+          text: 'Activities'
+        },
+        legend: {
+            labels: {
+                // This more specific font property overrides the global property
+                font: {
+                    size: 14,
+                    color:'#fff'
+                }
+            }
+        }
+      },
+      scales: {
+        x:{
+            ticks: {
+                color: "#fff", // not 'fontColor:' anymore
+                // fontSize: 18,
+                font: {
+                  size: 14, // 'size' now within object 'font {}'
+                }
+              }
+        },
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+          ticks: {
+            color: "#fff", // not 'fontColor:' anymore
+            // fontSize: 18,
+            font: {
+              size: 14, // 'size' now within object 'font {}'
+            },
+            beginAtZero: true
+          }
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+          ticks: {
+            color: "#fff", // not 'fontColor:' anymore
+            // fontSize: 18,
+            font: {
+              size: 14, // 'size' now within object 'font {}'
+            }
+          },
+  
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+      }
+}
+});
